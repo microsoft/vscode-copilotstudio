a057cff0022b6612fc5088fa00f3d9cddcefbbbc
Chris Mullins <cleemullins@users.noreply.github.com>
2025-03-20 16:55:16 -0700

    Add in test directory to eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import globals from "globals";
 
 export default [{
-    files: ["*.js"],    
+    files: ["*.js", "test/*.js"],    
     languageOptions: {
         globals: {
             ...globals.commonjs,
