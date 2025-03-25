827e759835080c26f8759a253cbf42a6aa6f3a0e
Chris Mullins <cleemullins@users.noreply.github.com>
2025-03-25 22:22:19 +0000

    Update setversion.js to export variable with package version. Enabled ADO Build stuff.

--- setVersion.js
+++ setVersion.js
@@ -1,2 +1,5 @@
 const nbgv = require('nerdbank-gitversioning')
 nbgv.setPackageVersion('.')
+nbgv.getVersion('.').then( (version)=> {
+    console.log('##vso[task.setvariable variable=CUSTOM_VERSION;]' + version.npmPackageVersion)
+})
