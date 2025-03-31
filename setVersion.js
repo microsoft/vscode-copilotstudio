6dd8728f84c06a83c3ae3d3b3dd523e99a87510b
Chris Mullins <cleemullins@users.noreply.github.com>
2025-03-31 11:01:36 -0700

    Update setVersion.js
    Added some explanation into the setVersion file so that it's clear why the ADO Environment variable is being set.

--- setVersion.js
+++ setVersion.js
@@ -1,5 +1,12 @@
 const nbgv = require('nerdbank-gitversioning')
 nbgv.setPackageVersion('.')
+
+// When this is run in the Azure Devops build pipeline, the build number is needed as an 
+// environment variable so the manifest and signing files can be created with the same name
+// as the VSIX itself. The odd syntax below exports the version number into an 
+// envionrment variable so it's easy to consume in the build. More details on the syntax can
+// be found here:
+// https://learn.microsoft.com/en-us/azure/devops/pipelines/process/set-variables-scripts?view=azure-devops&tabs=bash
 nbgv.getVersion('.')
     .then((version) => {
         console.log('##vso[task.setvariable variable=CUSTOM_VERSION;]' + version.npmPackageVersion)
